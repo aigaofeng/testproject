970c043ce3fd1fe2275632505ba48d7c0453ee54
aigaofeng <515748965@qq.com>
2018-09-30 15:01:00 +0800

    冲突更改

--- src/cn/utry/ws/service/jdk/HelloServiceImpl.java
+++ src/cn/utry/ws/service/jdk/HelloServiceImpl.java
@@ -8,11 +8,7 @@ public class HelloServiceImpl {
 	@WebMethod	//标记为远程服务远程方法
 	public String sayHello(String name) {
 		
-<<<<<<< HEAD
-		return name+"南方姑娘，你是否喜欢北方的秋凉";
-=======
-	
-	return name+"南方姑娘，你是否喜欢北方的秋凉2018/9/15  赵雷";
->>>>>>> f0fcc3a01767263bcbf692c0c85fdeeb89f68376
+
+	return name+"明天就是国庆节了，住大家节日快乐";
 	}
 }
